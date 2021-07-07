50dec9d4707e4a7d6064bc800a4c51709e8973c6
Hp <Hp@DESKTOP-49CS78G>
2021-07-07 09:11:39 +0530

    First commit

---
+++ src/demo/HelloSelenium.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class HelloSelenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello automation world");
+
+	}
+
+}
